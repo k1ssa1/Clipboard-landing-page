58576632cb9454acc5001fa61d598067a01376a0
k1ssa1 <samikissai.edu@gmail.com>
2024-07-04 23:08:10 +0200

    feat: add responsiveness to sponsors section

--- src/Components/Sponsors.jsx
+++ src/Components/Sponsors.jsx
@@ -6,12 +6,12 @@ import vector from '../assets/logo-vector-graphics.png'
 
 const Sponsors = () => {
     return (  
-        <div className='flex flex-row justify-around mt-24 mb-10 mx-14 py-10'>
-            <img src={google} alt="google" className='w-48 h-14'/>
-            <img src={ibm} alt="ibm" className='w-48 h-14'/>
-            <img src={microsoft} alt="microsoft" className='w-48 h-14'/>
-            <img src={hp} alt="hp" className='w-48 h-14'/>
-            <img src={vector} alt="vector" className='w-48 h-14'/>
+        <div className='flex flex-row justify-around mt-24 mb-10 mx-14 py-10 max-sm:flex-col max-sm:items-center'>
+            <img src={google} alt="google" className='w-48 h-14 max-sm:mb-10'/>
+            <img src={ibm} alt="ibm" className='w-48 h-14 max-sm:mb-10'/>
+            <img src={microsoft} alt="microsoft" className='w-48 h-14 max-sm:mb-10'/>
+            <img src={hp} alt="hp" className='w-48 h-14 max-sm:mb-10'/>
+            <img src={vector} alt="vector" className='w-48 h-14 max-sm:mb-10'/>
         </div>
     );
 }
