d5c159ea97436b5d553344a11f2162f09a3e96f1
k1ssa1 <samikissai.edu@gmail.com>
2024-07-04 21:46:47 +0200

    fix: update assets width

--- src/Components/Sponsors.jsx
+++ src/Components/Sponsors.jsx
@@ -6,12 +6,12 @@ import vector from '../assets/logo-vector-graphics.png'
 
 const Sponsors = () => {
     return (  
-        <div className='flex flex-row justify-around mt-24 mb-10 mx-14 p-10'>
-            <img src={google} alt="google" className='w-40 h-12'/>
-            <img src={ibm} alt="ibm" className='w-40 h-12'/>
-            <img src={microsoft} alt="microsoft" className='w-40 h-12'/>
-            <img src={hp} alt="hp" className='w-40 h-12'/>
-            <img src={vector} alt="vector" className='w-40 h-12'/>
+        <div className='flex flex-row justify-around mt-24 mb-10 mx-14 py-10'>
+            <img src={google} alt="google" className='w-48 h-14'/>
+            <img src={ibm} alt="ibm" className='w-48 h-14'/>
+            <img src={microsoft} alt="microsoft" className='w-48 h-14'/>
+            <img src={hp} alt="hp" className='w-48 h-14'/>
+            <img src={vector} alt="vector" className='w-48 h-14'/>
         </div>
     );
 }
