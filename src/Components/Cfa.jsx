4ba7674277745907c2b304e73c3e6fb9381521aa
k1ssa1 <samikissai.edu@gmail.com>
2024-07-04 23:10:12 +0200

    feat: add responsiveness to cla section

--- src/Components/Cfa.jsx
+++ src/Components/Cfa.jsx
@@ -1,10 +1,10 @@
 const Cfa = () => {
     return (  
         <div className="flex flex-col items-center justify-center text-center mt-32 mb-10">
-            <h6 className="text-3xl font-bold mb-4">Clipboard for iOS and Mac OS</h6>
+            <h6 className="text-3xl font-bold mb-4 max-sm:w-2/3">Clipboard for iOS and Mac OS</h6>
             <p className="w-1/2 mb-10 text-sm text-gray-400">Available for free on the App Store. Download for Mac or iOS, sync with iCloud and you're ready to start adding to your clipboard.</p>
-            <div>
-                <button className="mr-4 bg-emerald-500 py-2 px-6 rounded-3xl text-white shadow-md shadow-emerald-500">Donwload for iOS</button>
+            <div className="max-sm:flex max-sm:flex-col">
+                <button className="mr-4 bg-emerald-500 py-2 px-6 rounded-3xl text-white shadow-md shadow-emerald-500 max-sm:mb-8">Donwload for iOS</button>
                 <button className="mr-4 bg-blue-500 py-2 px-6 rounded-3xl text-white shadow-md shadow-blue-500">Download for Mac</button>
             </div>
         </div>
