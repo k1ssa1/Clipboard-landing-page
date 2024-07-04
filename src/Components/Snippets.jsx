fc225a4e2c6acc3c54a0420f4fab7caf686d21a5
k1ssa1 <samikissai.edu@gmail.com>
2024-07-04 22:51:06 +0200

    fix: computer asset position

--- src/Components/Snippets.jsx
+++ src/Components/Snippets.jsx
@@ -1,3 +1,5 @@
+import computer from '../assets/image-computer.png'
+
 const Snippets = () => {
     return ( 
         <div className="grid grid-cols-1 my-24">
@@ -5,19 +7,20 @@ const Snippets = () => {
                 <h4 className="text-2xl font-bold text-gray-900 max-sm:w-1/2">Keep track of your snippets</h4>
                 <p className="w-1/3 mt-5 text-sm text-gray-400 max-sm:w-1/2">Clipboard instantly stores any item you copy in the cloud, meaning you can access your snippets immediately on all your devices. Our Mac and iOS apps will help you organize everything</p>
             </div>
-            <div className="mt-24 bg-snipets bg-no-repeat bg-left bg-contain p-16">
-                <ul className="float-right mr-56 text-justify w-80">
+            <div className="grid grid-cols-2 py-5 items-center mt-16">
+                <img src={computer} alt="computer" className='relative right-10'/>
+                <ul className="ml-28">
                     <li className="mb-11">
                         <h6 className="mb-1 font-bold">Quick Search</h6>
-                        <p className="text-gray-400 text-sm">Easily search your snippets by content, category, web address, application, and more.</p>
+                        <p className="text-gray-400 text-sm w-80">Easily search your snippets by content, category, web address, application, and more.</p>
                     </li>
                     <li className="mb-11">
                         <h6 className="mb-1 font-bold">iCloud Sync</h6>
-                        <p className="text-gray-400 text-sm">Instantly saves and syncs snippets across all your devices.</p>
+                        <p className="text-gray-400 text-sm w-80">Instantly saves and syncs snippets across all your devices.</p>
                     </li>
                     <li>
                         <h6 className="mb-1 font-bold">Complete History</h6>
-                        <p className="text-gray-400 text-sm">Retrieve any snippets from the first moment you started using the app.</p>
+                        <p className="text-gray-400 text-sm w-80">Retrieve any snippets from the first moment you started using the app.</p>
                     </li>
                 </ul>
             </div>
